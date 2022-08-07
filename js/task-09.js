51f59323075065da99e569cb82ae43e4d83fe1a9
gubariev-vitalii <100084830+GubarievVitalii@users.noreply.github.com>
2022-08-07 21:59:35 +0300

    9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnChangeColor = document.querySelector('.change-color');
+const textColor = document.querySelector('.color');
+btnChangeColor.addEventListener('click', onChangeColor);
+
+function onChangeColor() {
+  const newColor = getRandomHexColor()
+  document.body.style.backgroundColor = newColor;
+  textColor.textContent = newColor;
+};
