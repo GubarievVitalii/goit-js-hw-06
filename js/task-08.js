b54d57cfe0a4d64ed46756090c9245a6dad2378d
gubariev-vitalii <100084830+GubarievVitalii@users.noreply.github.com>
2022-08-07 21:16:07 +0300

    8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const formEl = document.querySelector(".login-form");
+formEl.addEventListener('submit', onChangeForm);
 
+function onChangeForm(event) {
+    event.preventDefault();
+    const formArray = Array.from(document.querySelectorAll("input"));
+    const formValueArray = formArray.map((element) => element.value);
+
+    if (formValueArray.includes("")) {
+        return alert('Усі поля треба заповнити!')
+    };
+
+    const formData = new FormData(event.currentTarget);
+    const result = {};
+    formData.forEach((value, name) => (result[name] = value));
+    console.log(result);
+    event.currentTarget.reset();
+};
